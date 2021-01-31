54f5ed7cec45f7282245c560103b1914abcd32cd
HoffsMH <matthecker@pm.me>
2021-01-30 23:12:10 -0600

    no more polybar and use term instead of kitty

--- lib/startx.ex
+++ lib/startx.ex
@@ -6,12 +6,11 @@ defmodule Tl.Startx do
 
     log("run once")
 
-    Tl.Cmd.start("/usr/bin/kitty")
+    Tl.Cmd.start(Path.expand("~/bin/term"))
     Tl.Cmd.start("/usr/bin/brave")
     Tl.Cmd.start("/usr/bin/slack")
     Tl.Cmd.start("/usr/bin/google-chrome-stable")
     Tl.Cmd.exec("/usr/bin/feh", ["--bg-scale", Path.expand("~/.wall.jpg")])
-    Tl.Cmd.start(Path.expand("~/bin/wall_polybar"))
 
     log("starting long running commands")
     Tl.Startx.Supervisor.start_link(name: Tl.Startx.Supervisor)
